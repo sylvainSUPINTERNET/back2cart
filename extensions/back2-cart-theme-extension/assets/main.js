2849b5c9cc16e213ab7ccf5815425114549a8b35
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-09-17 23:48:16 +0200

    int

---
+++ extensions/back2-cart-theme-extension/assets/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let btnNotify = document.getElementById('back2cart-btn');
+
+    if ( btnNotify ) {
+        btnNotify.addEventListener('click', () => {
+            alert("ruck");
+        });
+    }
+});
