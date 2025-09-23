45c4ed9c50232812a69fb5252641cee2220c31ea
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-09-23 23:50:44 +0200

    wip

--- extensions/back2-cart-theme-extension/assets/main.js
+++ extensions/back2-cart-theme-extension/assets/main.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+    
     let btnNotify = document.getElementById('back2cart-btn');
+    let formSubscribe = document.getElementById('subscribe-form');
+
+    if ( formSubscribe ) {
+        formSubscribe.addEventListener('submit', (e) => {
+            e.preventDefault();
+            alert("Subscribed!");
+        });
+    }
 
     if ( btnNotify ) {
         btnNotify.addEventListener('click', () => {
