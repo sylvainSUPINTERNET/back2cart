82b23253b9a7299924b93b27618781444a6f86a4
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2025-09-24 23:35:39 +0200

    wip

--- extensions/back2-cart-theme-extension/assets/main.js
+++ extensions/back2-cart-theme-extension/assets/main.js
@@ -4,8 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let formSubscribe = document.getElementById('subscribe-form');
 
     if ( formSubscribe ) {
-        formSubscribe.addEventListener('submit', (e) => {
+        formSubscribe.addEventListener('submit', async (e) => {
             e.preventDefault();
+
+            const resp = await fetch('http://localhost:3000/posts');
+            const data = await resp.json();
+            console.log(data);
+
             alert("Subscribed!");
         });
     }
